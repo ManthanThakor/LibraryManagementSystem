1eb85e064dfdf47a3424f61380046a5c4abc214c
ManthanThakor <thakormanthan849@gmail.com>
2024-11-18 18:07:10 +0530

    change

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LibraryManagmentSystem
+{
+
+    public class Author
+    {
+        public string Name { get; set; }
+        public string Bio { get; set; }
+
+        public Author(string name, string bio)
+        {
+            Name = name;
+            Bio = bio;
+        }
+    }
+
+    public class Book
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class Library
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\"LIBRARY MANAGMENT SYSTEM\"");
+
+           
+
+            Console.ReadLine();
+        }
+    }
+}
